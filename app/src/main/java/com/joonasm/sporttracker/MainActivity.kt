8ca137fc70552e63e8e552a11aa0ff591aa4f848
Joonas <mielonen.joonas@hotmail.com>
2021-10-01 17:52:55 +0300

    Basic bottom navigation

--- app/src/main/java/com/joonasm/sporttracker/MainActivity.kt
+++ app/src/main/java/com/joonasm/sporttracker/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.joonasm.sporttracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.joonasm.sporttracker.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHost.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.mapFragment, R.id.profileFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val navView = binding.bottomNavigationView
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/joonasm/sporttracker/MapFragment.kt
@@ -0,0 +1,7 @@
+package com.joonasm.sporttracker
+
+import androidx.fragment.app.Fragment
+
+class MapFragment : Fragment(R.layout.fragment_map) {
+
+}
